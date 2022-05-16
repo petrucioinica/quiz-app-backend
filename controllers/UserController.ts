a6d9c318c43f125c2c832a6d70f7465478ef761b
petrucioinica <cioinicapetru@gmail.com>
2022-05-16 18:11:10 +0300

    created required middleware

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import { nextTick } from "process";
+const requiredHandler = require("../middlewares/requiredHandler");
 const express = require("express");
 const router: Router = express.Router();
 
@@ -8,9 +9,13 @@ router.use((req, res, next) => {
 	next();
 });
 
-router.post("/register", (req, res) => {
-	const body = req.body;
-	res.status(200).json(body);
-});
+router.post(
+	"/register",
+	requiredHandler(["username", "password", "email"]),
+	(req, res) => {
+		const body = req.body;
+		res.status(200).json(body);
+	}
+);
 
 module.exports = router;
---
+++ middlewares/requiredHandler.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+//middleware that handles required
+//it recieves an array of required fields. If the body does not have those fields it returns a 400 with the appropriate message
+module.exports = (fields: string[]) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+		const body = req.body;
+
+		for (const field of fields) {
+			if (body[field] === undefined || body[field] === "") {
+				res.status(400).json({
+					error: "Error! Field is required!",
+					message: `Error! ${field} is required!`,
+				});
+				break;
+			}
+		}
+		next();
+	};
+};
